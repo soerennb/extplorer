48d48cda056cd7b79fd76a869aac4b3cf65aae91
soeren <soerennb@users.noreply.github.com>
2012-11-21 20:42:49 +0000

    adjustments for Joomla! 3.0

--- install.extplorer.php
+++ install.extplorer.php
@@ -9,13 +9,15 @@ if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' 
 * @author soeren
 * eXtplorer is Free Software
 */
-function com_install(){
+function com_install($mypath =''){
 	global $database;
 	
 	if( is_callable( array( 'JFactory', 'getDBO' ))) {
 		$database = JFactory::getDBO();
 	}
-	$mypath = dirname(__FILE__);
+	if( $mypath == '' ) {
+		$mypath = dirname(__FILE__);
+	}
 	require_once($mypath . "/include/functions.php");
 	require_once($mypath . "/libraries/Archive/archive.php");
 	
@@ -43,4 +45,39 @@ function com_install(){
 		$database->query();
 	}
 }
-?>
+class com_extplorerInstallerScript
+{
+ 
+        /**
+         * Called on installation
+         *
+         * @param   JAdapterInstance  $adapter  The object responsible for running this script
+         *
+         * @return  boolean  True on success
+         */
+        public function install( $adapter){
+			$adminpath = JPATH_SITE.'/administrator/components/com_extplorer';
+			com_install($adminpath);
+			rename($adminpath.'/extplorer.j30.php', $adminpath.'/extplorer.php');
+			return true;
+		}
+        /**
+         * Called on update
+         *
+         * @param   JAdapterInstance  $adapter  The object responsible for running this script
+         *
+         * @return  boolean  True on success
+         */
+        public function update( $adapter) {
+			return true;
+		}
+ 
+        /**
+         * Called on uninstallation
+         *
+         * @param   JAdapterInstance  $adapter  The object responsible for running this script
+         */
+        public function uninstall( $adapter) {
+			return true;
+		}
+}
