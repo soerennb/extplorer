6a99e201ed23b9d2fd13d65241075d38b173a16f
Sören <info@extplorer.net>
2019-05-15 11:40:38 +0200

    fixed "deprecated" notice

--- libraries/FTP.php
+++ libraries/FTP.php
@@ -31,7 +31,7 @@ require_once 'PEAR.php';
  * @name NET_FTP_FILES_ONLY
  * @see Net_FTP::ls()
  */
-define('NET_FTP_FILES_ONLY', 0, true);
+define('NET_FTP_FILES_ONLY', 0);
 
 /**
  * Option to let the ls() method return only directories.
@@ -40,7 +40,7 @@ define('NET_FTP_FILES_ONLY', 0, true);
  * @name NET_FTP_DIRS_ONLY
  * @see Net_FTP::ls()
  */
-define('NET_FTP_DIRS_ONLY', 1, true);
+define('NET_FTP_DIRS_ONLY', 1);
 
 /**
  * Option to let the ls() method return directories and files (default).
@@ -49,7 +49,7 @@ define('NET_FTP_DIRS_ONLY', 1, true);
  * @name NET_FTP_DIRS_FILES
  * @see Net_FTP::ls()
  */
-define('NET_FTP_DIRS_FILES', 2, true);
+define('NET_FTP_DIRS_FILES', 2);
 
 /**
  * Option to let the ls() method return the raw directory listing from ftp_rawlist()
@@ -58,7 +58,7 @@ define('NET_FTP_DIRS_FILES', 2, true);
  * @name NET_FTP_RAWLIST
  * @see Net_FTP::ls()
  */
-define('NET_FTP_RAWLIST', 3, true);
+define('NET_FTP_RAWLIST', 3);
 
 /**
  * Option to indicate that non-blocking features should not be used in
@@ -68,7 +68,7 @@ define('NET_FTP_RAWLIST', 3, true);
  * @name NET_FTP_BLOCKING
  * @see Net_FTP::put()
  */
-define('NET_FTP_BLOCKING', 1, true);
+define('NET_FTP_BLOCKING', 1);
 
 /**
  * Option to indicate that non-blocking features should be used if available in
@@ -80,7 +80,7 @@ define('NET_FTP_BLOCKING', 1, true);
  * @name NET_FTP_NONBLOCKING
  * @see Net_FTP::put()
  */
-define('NET_FTP_NONBLOCKING', 2, true);
+define('NET_FTP_NONBLOCKING', 2);
 
 /**
  * Error code to indicate a failed connection
