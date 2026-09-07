dfa1605237dfbfebc09499b9848895746639f922
soeren <soeren@sefoto.de>
2026-09-07 15:59:41 +0200

    chore: prepare 3.0.0-beta.4 release

--- app/Config/App.php
+++ app/Config/App.php
@@ -264,7 +264,7 @@ class App extends BaseConfig
      * Application Version
      * --------------------------------------------------------------------------
      */
-    public string $version = '3.0.0-beta.3';
+    public string $version = '3.0.0-beta.4';
 
     private function configureTrustedProxies(): void
     {
